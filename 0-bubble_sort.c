7d24b0291b2db6fb3472e24990ff60e8c6b93ddf
samuelsrmv <samuel.srmv@gmail.com>
2021-02-02 10:33:52 -0500

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,45 +1,47 @@
 #include "sort.h"
-/**
-* cambio: change value
-* @a: pointer
-* @b: pointer
-*/
 
-void cambio(int *a, int *b)
+/**
+ * swap - swap values
+ * @a: value a
+ * @b: value b
+ */
+void swap(int *a, int *b)
 {
-    int tmp = *b;
-    
-    *b = *a;
-    *a = tmp;
+	int tmp = *b;
+
+	*b = *a;
+	*a = tmp;
 }
 /**
-* bubble_sort - bubble sort
-* @array: array
-* @size: size
-*/
+ * bubble_sort - sort array using bubble
+ * @array: array to sorting
+ * @size: size of array
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, x;
-    {
-        for (i = 0; i < size; i++)
-        {    
-            for (j = 0; j < size; j++)
-            {
-                if (array[j] > array[j + 1])
-                {
-                    cambio(&array[j], &array[j + 1]);
-                    for (x = 0; x < size; x++)
-                    {
-                    printf("%d", array[x]);
-                    if (x !=  size - 1)
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                    }
-                    putchar('\n');
-                }
-            }
-        }
-    }
+	size_t i, j, x;
+
+	if (size >= 2)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			for (j = 0; j < size - 1 - i; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					for (x = 0; x < size; x++)
+					{
+					printf("%d", array[x]);
+					if (x != size - 1)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+					putchar('\n');
+				}
+			}
+		}
+	}
 }
